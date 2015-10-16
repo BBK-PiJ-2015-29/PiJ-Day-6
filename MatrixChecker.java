3a3998a4624e24eb9c52bbd78f4eb5e6cef2ba89
unknown <ocouls01@CSPC025.dcs.bbk.ac.uk>
2015-10-16 17:45:45 +0100

    Started exercise 5, created MatrixChecker class and started first method.

---
+++ MatrixChecker.java
@@ -0,0 +1,10 @@
+public class MatrixChecker {
+
+	public boolean isSymmetrical(int[] input) {
+		//to check if an array is symmetrical, invert it and then compare
+		int[] test = new int[input.length];
+		for (int i = input.length-1; i >= 0; i--) {
+			for (int j = 0; j < input.length; j++)
+		}
+	}
+}
