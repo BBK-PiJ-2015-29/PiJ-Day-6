7c43015d690199b8c1e578c48ec2626003f7c915
Ollie Coulson <olliecoulson@Ollies-MBP.home>
2015-10-18 16:31:44 +0100

    added isSymmetrical  methods for 1d and 2d int arrays in MatrixChecker.java, also ammended Ex3.java to add calls for these methods to test them.

--- Ex3.java
+++ Ex3.java
@@ -1,6 +1,7 @@
 public class Ex3 {
 	public static void main(String[] args) {
 		Matrix m1 = new Matrix(3, 3);
+		MatrixChecker check = new MatrixChecker();
 		
 		System.out.println("We have an integer Matrix of 3 by 3");
 
@@ -13,6 +14,8 @@ public class Ex3 {
 			System.out.println("4 - print the matrix as a single line");
 			System.out.println("5 - print the matrix as a grid");
 			System.out.println("6 - add an entire matrix from a string");
+			System.out.println("7 - check if an int[] input is symmetrical");
+			System.out.println("8 - check if an int[][] input is symmetrical");
 			System.out.println("0 - Exit");
 			System.out.print("> ");
 			
@@ -22,6 +25,7 @@ public class Ex3 {
 			int col = 0;
 			String rowValues = "";
 			String colValues = "";
+			boolean symmetrical = true;
 			switch (choice) {
 				case 0: 
 					System.exit(0);
@@ -87,6 +91,41 @@ public class Ex3 {
 					input = System.console().readLine();
 					m1.setMatrix(input);
 					break;
+					
+				case 7:
+					System.out.println("Please input int[] to be checked,ints separated by commas");
+					System.out.print("> ");
+					input = System.console().readLine();
+					
+					String[] holder = input.split(",");
+					int[] inputIntArray = new int[holder.length];
+					for (int i = 0; i < holder.length; i++) {
+						inputIntArray[i] = Integer.parseInt(holder[i]);
+					}
+					
+					symmetrical = check.isSymmetrical(inputIntArray);
+					if (symmetrical) {
+						System.out.println("The input int[] is symmetrical\n");
+					}
+					else {
+						System.out.println("The input int[] is not symmetrical\n");
+					}
+					break;
+					
+				case 8:
+					System.out.println("Input int[][], elements separated by \',\'s and\nrows separated by \';\'s");
+					System.out.print("> ");
+					input = System.console().readLine();
+					m1.setMatrix(input);
+					symmetrical = check.isSymmetrical(m1.getArray());
+					if (symmetrical) {
+						System.out.println("The input int[] is symmetrical\n");
+					}
+					else {
+						System.out.println("The input int[] is not symmetrical\n");
+					}
+					break;
+					
 				default:
 					System.out.println("Not a valid input");
 					
--- MatrixChecker.java
+++ MatrixChecker.java
@@ -1,10 +1,30 @@
 public class MatrixChecker {
 
 	public boolean isSymmetrical(int[] input) {
-		//to check if an array is symmetrical, invert it and then compare
-		int[] test = new int[input.length];
-		for (int i = input.length-1; i >= 0; i--) {
-			for (int j = 0; j < input.length; j++)
+		//to check if an array is symmetrical, compare the value at index 0 with the value
+		//at index length-1, then the value at index 1 with the value at index length-2 etc.
+		
+		boolean symmetrical = true;
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] != input[(input.length - 1) - i]) {
+				symmetrical = false;
+				break;
+			}
+			
 		}
+		return symmetrical;
+	}
+	
+	public boolean isSymmetrical(int[][] input) {
+		boolean symmetrical = true;
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < input[0].length; j++) {
+				if (input[i][j] != input[j][i]) {
+					symmetrical = false;
+					break;
+				}
+			}	
+		}
+		return symmetrical;
 	}
 }
